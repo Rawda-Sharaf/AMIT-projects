ec52f9e77d73ab4bd2ea991c7f33ba404f226411
Rawda-Sharaf <rawdasharaf11@gmail.com>
2025-01-26 02:57:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import helper
 import streamlit as st
 import pickle
 
-model = pickle.load(open('C:/Users/Right Click/Desktop/AMIT AI/final projects/project1/artifacts/dt.pkl', 'rb'))
+model = pickle.load(open('artifacts/dt.pkl', 'rb'))
 
 text = st.text_input("Enter your text here")
 
